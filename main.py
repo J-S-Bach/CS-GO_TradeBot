0cb25b63f31590549d1b375764a2ec89b961da0e
TobiasHeise <tobias.heise01@web.de>
2023-02-23 15:25:08 +0100

    wrote initial api handler

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from api.buff import api
 def main():
     buffMarketplace = api.BuffMarketplace()
     buffMarketplace.getItemDetail(name="Glove Case")
-
+t
 
 if __name__ == '__main__':
     main()
